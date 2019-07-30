1b7a1375cbe1ccbb93f9cd8cd4b78216da2800c1
luciamoreno2019 <lmoreno@altimetrik.com>
2019-07-30 14:38:24 -0300

    Logic inside the helper.

--- project/unpackaged/myPackage/aura/TimesheetFilter/TimesheetFilterController.js
+++ project/unpackaged/myPackage/aura/TimesheetFilter/TimesheetFilterController.js
@@ -1,32 +1,18 @@
 ({
     filter : function (component, event) {
-        component.set('v.filter', true);
-        var filter = component.get("v.filter");
-        var updateEvent = component.getEvent("updateFilter");
-        updateEvent.setParams({ "filter1": filter });
-        updateEvent.fire();
+        helper.helperFilter(component, event);
         
     },
     
     all : function (component) {
-        component.set('v.all', true);
-        var all = component.get("v.all");
-        var updateEvent = component.getEvent("updateFilter");
-        updateEvent.setParams({ "all": all });
-        updateEvent.fire();
+        helper.helperAll(component);
     },
+    
     submitted : function (component) {
-        component.set('v.submitted', true);
-        var submitted = component.get("v.submitted");
-        var updateEvent = component.getEvent("updateFilter");
-        updateEvent.setParams({ "submitted": submitted });
-        updateEvent.fire();
+        helperSubmitted(component);
     },
+
     notSubmitted : function (component) {
-        component.set('v.notSubmitted', true);
-        var notSubmitted = component.get("v.notSubmitted");
-        var updateEvent = component.getEvent("updateFilter");
-        updateEvent.setParams({ "notSubmitted": notSubmitted });
-        updateEvent.fire();
+        helperNotSubmitted(component);
     }
 })
--- project/unpackaged/myPackage/aura/TimesheetFilter/TimesheetFilterHelper.js
+++ project/unpackaged/myPackage/aura/TimesheetFilter/TimesheetFilterHelper.js
@@ -1,5 +1,35 @@
 ({
-	helperMethod : function() {
-		
-	}
+	
+	helperFilter : function (component, event) {
+        component.set('v.filter', true);
+        var filter = component.get("v.filter");
+        var updateEvent = component.getEvent("updateFilter");
+        updateEvent.setParams({ "filter1": filter });
+        updateEvent.fire();
+        
+	},
+	
+	helperAll : function (component) {
+        component.set('v.all', true);
+        var all = component.get("v.all");
+        var updateEvent = component.getEvent("updateFilter");
+        updateEvent.setParams({ "all": all });
+        updateEvent.fire();
+	},
+	
+	helperSubmitted : function (component) {
+        component.set('v.submitted', true);
+        var submitted = component.get("v.submitted");
+        var updateEvent = component.getEvent("updateFilter");
+        updateEvent.setParams({ "submitted": submitted });
+        updateEvent.fire();
+	},
+	
+	helperNotSubmitted : function (component) {
+        component.set('v.notSubmitted', true);
+        var notSubmitted = component.get("v.notSubmitted");
+        var updateEvent = component.getEvent("updateFilter");
+        updateEvent.setParams({ "notSubmitted": notSubmitted });
+        updateEvent.fire();
+    }
 })
